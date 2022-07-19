62580075a03448bcdf7d5ff628aaeb6f25185579
Mohamad Reza <89145440+mrNitroPy@users.noreply.github.com>
2022-07-19 16:51:51 +0430

    Add files via upload

--- api.py
+++ api.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-#-----------------------------------
-#                                   #
-# »Download Nohe as site«           # 
-# programmer : Mohammad Lakzaee     #
-# Version : 0.2                     #
-#-----------------------------------#
-
 import requests
 import re
 from bs4 import BeautifulSoup
 import random
+import os
 
 print("Start Downlaoding Nohe ....")
 page = requests.get("https://nicmusic.net/%D9%85%D8%AF%D8%A7%D8%AD%DB%8C-%D8%AC%D8%AF%DB%8C%D8%AF/").content
 
 soup = BeautifulSoup(page, 'html.parser')
 
+if os.path.isdir("nohes") != True:
+	os.mkdir("nohes")
 
 for link in soup.find_all('a', attrs={'href': re.compile("mp3"), 'class': 'dl-320'}):
     nohe = link.get('href')
